e58d533284e78ebbe0c8b0c12741f5b9189b61fd
Makara Wang <makara15@gmail.com>
2011-04-16 12:22:14 +0800

    no more symlinks

--- rebuild.sh
+++ rebuild.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 DIRS="contrib developer multilingual performance";
 for DIR in $DIRS; do
-  if [[ -d "modules/$DIR" ]]; then
+  if [ -d "modules/$DIR" ]; then
     rm -rf "modules/$DIR";
   fi;
 done;
 
 DIRS="tao rubik";
 for DIR in $DIRS; do
-  if [[ -d "themes/$DIR" ]]; then
+  if [ -d "themes/$DIR" ]; then
     rm -rf "themes/$DIR";
   fi;
 done;
 
-drush make --no-core --contrib-destination=. config/example.make -y;
+drush make --no-core --contrib-destination=html/sites/all config/example.make -y;
+
