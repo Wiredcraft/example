25cca3d9332880e1cb46967db97b9847f9767dda
Makara Wang <makara15@gmail.com>
2011-04-25 00:15:44 +0800

    fix sh

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@
 drush make --no-core --contrib-destination=temp_all config/example.make -y;
 
 if [ ! -d html/sites/all ]; then
-	mkdir html/sites/all;
+	mkdir -p html/sites/all;
 fi;
 
 case "$1" in
@@ -27,11 +27,14 @@ case "$SELECTION" in
 		fi;
 		echo -e "Replacing all...\c";
 		for DIR in `ls temp_all`; do
+			if [ ! -d "html/sites/all/$DIR" ]; then
+				 mkdir -p "html/sites/all/$DIR";
+			fi;
 			for SUB_DIR in `ls "temp_all/$DIR"`; do
 				if [ -d "html/sites/all/$DIR/$SUB_DIR" ]; then
 					 rm -rf "html/sites/all/$DIR/$SUB_DIR";
 				fi;
-				cp -R "temp_all/$DIR/$SUB_DIR" "html/sites/all/$DIR/$SUB_DIR";
+				cp -R "temp_all/$DIR/$SUB_DIR" "html/sites/all/$DIR/";
 				echo -e ".\c";
 			done;
 		done;
@@ -39,6 +42,3 @@ case "$SELECTION" in
 		echo "done.";
 		;;
 esac
-
-
-
